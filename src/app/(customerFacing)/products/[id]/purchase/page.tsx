16bd48bd5b45bdb560399c140ab28454a35f2acb
David R <elidovrich.coding@gmail.com>
2025-03-15 09:32:34 -0400

    fix netlify deployment error

--- src/app/(customerFacing)/products/[id]/purchase/page.tsx
+++ src/app/(customerFacing)/products/[id]/purchase/page.tsx
@@ -3,27 +3,32 @@ import { notFound } from "next/navigation"
 import Stripe from "stripe"
 import CheckoutForm from "./_components/CheckoutForm"
 
-
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string)
 
+// Define the params interface
+type PurchasePageParams = {
+  params: {
+    id: string
+  }
+}
+
 export default async function PurchasePage({
-  params: { id },
-}: {
-  params: { id: string }
-}) {
+  params,
+}: PurchasePageParams) {
+  const { id } = params
   const product = await db.product.findUnique({ where: { id } })
   if (product == null) return notFound()
-
+  
   const paymentIntent = await stripe.paymentIntents.create({
     amount: product.priceInCents,
     currency: "USD",
     metadata: { productId: product.id },
   })
-
+  
   if (paymentIntent.client_secret == null) {
     throw Error("Stripe failed to create payment intent")
   }
-
+  
   return (
     <CheckoutForm
       product={product}
