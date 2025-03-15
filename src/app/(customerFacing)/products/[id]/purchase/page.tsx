e0ba4b249b1217ad4c9661e7fbc79ee07b720d00
David R <elidovrich.coding@gmail.com>
2025-03-15 09:27:16 -0400

    fix netlify deployment error

--- src/app/(customerFacing)/products/[id]/purchase/page.tsx
+++ src/app/(customerFacing)/products/[id]/purchase/page.tsx
@@ -1,29 +1,27 @@
-import db from "@/db/db";
-import { notFound } from "next/navigation";
-import Stripe from "stripe";
-import CheckoutForm from "./_components/CheckoutForm";
+import db from "@/db/db"
+import { notFound } from "next/navigation"
+import Stripe from "stripe"
+import CheckoutForm from "./_components/CheckoutForm"
 
-interface PageProps {
-  params: {
-    id: string;
-  };
-  searchParams?: Record<string, string | string[] | undefined>;
-}
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string)
 
-export default async function PurchasePage({ params: { id } }: PageProps) {
-  const product = await db.product.findUnique({ where: { id } });
-  if (product == null) return notFound();
+export default async function PurchasePage({
+  params: { id },
+}: {
+  params: { id: string }
+}) {
+  const product = await db.product.findUnique({ where: { id } })
+  if (product == null) return notFound()
 
   const paymentIntent = await stripe.paymentIntents.create({
     amount: product.priceInCents,
     currency: "USD",
     metadata: { productId: product.id },
-  });
+  })
 
   if (paymentIntent.client_secret == null) {
-    throw Error("Stripe failed to create payment intent");
+    throw Error("Stripe failed to create payment intent")
   }
 
   return (
@@ -31,5 +29,5 @@ export default async function PurchasePage({ params: { id } }: PageProps) {
       product={product}
       clientSecret={paymentIntent.client_secret}
     />
-  );
+  )
 }
